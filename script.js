6a79dd4ae8d16325ff456b8f8123f10ecd922976
Izentropowy <119621854+Izentropowy@users.noreply.github.com>
2024-01-31 22:52:38 +0100

    Add some other functions to linked list

--- script.js
+++ script.js
@@ -36,6 +36,49 @@ function createLinkedList() {
     size += 1;
   }
 
+  function at(index) {
+    let searchedNode = head;
+    for (let i = 0; i < index; i++) {
+      if (!searchedNode.nextNode) return null;
+      searchedNode = searchedNode.nextNode;
+    }
+
+    return searchedNode;
+  }
+
+  function pop() {
+    let beforeLast = at(size - 2);
+    let popped = tail;
+    beforeLast.nextNode = null;
+    tail = beforeLast;
+    size -= 1;
+    return popped;
+  }
+
+  function contains(value) {
+    let current = head;
+    for (let i = 0; i < size; i++) {
+      if (current.value === value) return true;
+      current = current.nextNode;
+    }
+
+    return false;
+  }
+
+  function find(value) {
+    let current = head;
+    for (let i = 0; i < size; i++) {
+      if (current.value === value) return i;
+      current = current.nextNode;
+    }
+
+    return null;
+  }
+
+  function toString() {
+    console.log(JSON.stringify(head));
+  }
+
   return {
     get head() {
       return head;
@@ -48,19 +91,23 @@ function createLinkedList() {
     },
     append,
     prepend,
+    at,
+    pop,
+    contains,
+    find,
+    toString,
   };
 }
 
 let linked = createLinkedList();
+
+linked.append(3);
 linked.append(4);
-console.log(linked.head);
-console.log(linked.tail);
-console.log("----");
-linked.append(40);
-console.log(linked.head);
-console.log(linked.tail);
-console.log("----");
-linked.prepend(3);
-console.log(linked.head);
-console.log(linked.tail);
-console.log("----");
+linked.append(5);
+linked.append("dupa");
+linked.append(3);
+linked.append(4);
+linked.append(5);
+linked.append("dupa");
+
+linked.toString();
